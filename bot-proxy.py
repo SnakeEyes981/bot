0c5c1ed57e78bb74345303ba4ec84b9a5af72d9d
Hassan Yaseen <hassanyaseenworks@gmail.com>
2024-09-29 07:54:15 -0500

    fixed TG message issue

--- bot-proxy.py
+++ bot-proxy.py
@@ -64,20 +64,20 @@ class Moonbix:
 
                         process_play_game(token=token, proxies=proxies)
 
-                        get_info(token=token, proxies=proxies)
+                        x, y, balance = get_info(token=token, proxies=proxies)
+                        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+                        message = f"Total Balance is: {balance:,} (Timestamp: {timestamp} UTC)"
+                        send_telegram_message(message)
 
                     else:
                         base.log(f"{base.red}Token not found! Please get new query id")
                 except Exception as e:
                     base.log(f"{base.red}Error: {base.white}{e}")
 
-            x, y, balance = get_info(token=token, proxies=proxies)
+            
             print()
             wait_time = 30 * 60
             base.log(f"{base.yellow}Wait for {int(wait_time/60)} minutes!")
-            timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-            message = f"Total Balance is: {balance:,} (Timestamp: {timestamp} UTC)"
-            send_telegram_message(message)
             time.sleep(wait_time)
 
 
